87dac2fb6bb1dd5e3375f843dbbe034bb75d06a5
Geral <105022840+Geral271@users.noreply.github.com>
2022-10-25 18:28:27 -0500

    Arquitectura limpia

---
+++ Trabajo2Web/Trabajo2Web/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+//importar entidades 
+
+namespace Trabajo2Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       // public DbSet<Genero> Generos { get; set; }
+
+    }
+}
